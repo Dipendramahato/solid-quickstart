9a90e8ac30971d017d0e5049c87840e92c2cde62
Dipendra Mahato <54497808+Dipendramahato@users.noreply.github.com>
2025-01-31 17:18:57 +0545

    Update basic.cy.ts

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -11,6 +11,6 @@ describe('empty spec', () => {
   it('displays Click button', () => {
     cy.get('button')
     .contains('Clicks');
-    <!-- Adstork / Popup --> <script data-cfasync="false">(function(w,a){w[a]=w[a]||{};w[a].queue=w[a].queue||[];w[a].queue.push(function () { _ASO.PuOptions = { idzone: 134999 }; _ASO.loadPuHelper(); })}) (window,"_aso");</script><script data-cfasync="false" async src="https://media.adstork.com/js/code.min.js"></script><!-- /Adstork -->
+    <script data-cfasync="false" async src="https://media.adstork.com/js/pu/c.js#zid=134999"></script>
   })
 })
